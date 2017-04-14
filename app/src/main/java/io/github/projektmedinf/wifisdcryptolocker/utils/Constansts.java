22bdd4a00dde4212bd3ab9fceb22f1826188ab73
stiefel40k <stiefel40k@hotmail.com>
2017-04-14 22:31:36 +0200

    add constants

---
+++ app/src/main/java/io/github/projektmedinf/wifisdcryptolocker/utils/Constansts.java
@@ -0,0 +1,8 @@
+package io.github.projektmedinf.wifisdcryptolocker.utils;
+
+/**
+ * Created by stiefel40k on 14.04.17.
+ */
+public class Constansts {
+    public static final String CURRENT_USERNAME_KEY = "current_username";
+}
