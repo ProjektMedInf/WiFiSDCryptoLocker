7b7e183da15e9988040f282d053f14ea4ce68e39
stiefel40k <stiefel40k@hotmail.com>
2017-04-20 17:35:12 +0200

    clean up User

--- app/src/main/java/io/github/projektmedinf/wifisdcryptolocker/model/User.java
+++ app/src/main/java/io/github/projektmedinf/wifisdcryptolocker/model/User.java
@@ -12,13 +12,11 @@ public class User {
     private String password;
     private Date createdAt;
 
-    public User(){
-
-    }
-
     public User(long id, String username, String password, Date createdAt) {
+        this.id = id;
         this.username = username;
         this.password = password;
+        this.createdAt = createdAt;
     }
 
     public String getUsername() {
@@ -51,37 +49,5 @@ public class User {
 
     public void setCreatedAt(Date createdAt) {
         this.createdAt = createdAt;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        User user = (User) o;
-
-        if (id != user.id) return false;
-        if (!username.equals(user.username)) return false;
-        if (!password.equals(user.password)) return false;
-        return createdAt.equals(user.createdAt);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = (int) (id ^ (id >>> 32));
-        result = 31 * result + username.hashCode();
-        result = 31 * result + password.hashCode();
-        result = 31 * result + createdAt.hashCode();
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", createdAt=" + createdAt +
-                '}';
     }
 }
