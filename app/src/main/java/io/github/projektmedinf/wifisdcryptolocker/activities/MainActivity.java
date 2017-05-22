cf2540865889f8ce9261c2d79e34d9d6c208fc31
GeorgB <georg.bernold@gmx.at>
2017-05-22 10:11:47 +0200

    Add fragments to main activity (#51)
    * closes 50

--- app/src/main/java/io/github/projektmedinf/wifisdcryptolocker/activities/MainActivity.java
+++ app/src/main/java/io/github/projektmedinf/wifisdcryptolocker/activities/MainActivity.java
@@ -2,7 +2,10 @@ package io.github.projektmedinf.wifisdcryptolocker.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -10,46 +13,178 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import io.github.projektmedinf.wifisdcryptolocker.R;
+import io.github.projektmedinf.wifisdcryptolocker.fragments.GalleryFragment;
+import io.github.projektmedinf.wifisdcryptolocker.fragments.HomeFragment;
+import io.github.projektmedinf.wifisdcryptolocker.fragments.ImportFragment;
 import io.github.projektmedinf.wifisdcryptolocker.model.User;
 
 import static io.github.projektmedinf.wifisdcryptolocker.utils.Constansts.CURRENT_USER_KEY;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG_HOME = "home";
+    private static final String TAG_IMPORT = "import";
+    private static final String TAG_GALLERY = "gallery";
+    public static int navItemIndex = 0;
+    public static String CURRENT_TAG = TAG_HOME;
+
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
+    private String[] activityTitles;
+    private boolean shouldLoadHomeFragOnBackPress = true;
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
+        mHandler = new Handler();
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        activityTitles = getResources().getStringArray(R.array.nav_item_activity_titles);
         Intent currentIntent = getIntent();
-        NavigationView navigationHeaderView = (NavigationView) findViewById(R.id.nav_view);
-        TextView menuUsernameTextView = (TextView) navigationHeaderView.getHeaderView(0).findViewById(R.id.menu_username_textView);
+        setUpNavigationView();
+        if (savedInstanceState == null) {
+            navItemIndex = 0;
+            CURRENT_TAG = TAG_HOME;
+            loadHomeFragment();
+        }
+        TextView menuUsernameTextView = (TextView) navigationView.getHeaderView(0).findViewById(R.id.menu_username_textView);
         User loggedInUser = currentIntent.getParcelableExtra(CURRENT_USER_KEY);
         menuUsernameTextView.setText(loggedInUser.getUsername());
     }
 
+    /***
+     * Returns respected fragment that user
+     * selected from navigation menu
+     */
+    private void loadHomeFragment() {
+        selectNavMenu();
+        setToolbarTitle();
+        if (getSupportFragmentManager().findFragmentByTag(CURRENT_TAG) != null) {
+            drawer.closeDrawers();
+            return;
+        }
+        Runnable mPendingRunnable = new Runnable() {
+            @Override
+            public void run() {
+                // update the main content by replacing fragments
+                Fragment fragment = getHomeFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,
+                        android.R.anim.fade_out);
+                fragmentTransaction.replace(R.id.frame, fragment, CURRENT_TAG);
+                fragmentTransaction.commitAllowingStateLoss();
+            }
+        };
+        if (mPendingRunnable != null) {
+            mHandler.post(mPendingRunnable);
+        }
+        drawer.closeDrawers();
+        invalidateOptionsMenu();
+    }
+
+    private Fragment getHomeFragment() {
+        switch (navItemIndex) {
+            case 0:
+                // home
+                HomeFragment homeFragment = new HomeFragment();
+                return homeFragment;
+            case 1:
+                // import
+                ImportFragment importFragment = new ImportFragment();
+                return importFragment;
+            case 2:
+                // gallery
+                GalleryFragment galleryFragment = new GalleryFragment();
+                return galleryFragment;
+            default:
+                return new HomeFragment();
+        }
+    }
+
+    private void setToolbarTitle() {
+        getSupportActionBar().setTitle(activityTitles[navItemIndex]);
+    }
+
+    private void selectNavMenu() {
+        navigationView.getMenu().getItem(navItemIndex).setChecked(true);
+    }
+
+    private void setUpNavigationView() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_home:
+                        navItemIndex = 0;
+                        CURRENT_TAG = TAG_HOME;
+                        break;
+                    case R.id.nav_import:
+                        navItemIndex = 1;
+                        CURRENT_TAG = TAG_IMPORT;
+                        break;
+                    case R.id.nav_gallery:
+                        navItemIndex = 2;
+                        CURRENT_TAG = TAG_GALLERY;
+                        break;
+                    default:
+                        navItemIndex = 0;
+                        break;
+                }
+                if (menuItem.isChecked()) {
+                    menuItem.setChecked(false);
+                } else {
+                    menuItem.setChecked(true);
+                }
+                menuItem.setChecked(true);
+                loadHomeFragment();
+                return true;
+            }
+        });
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.openDrawer, R.string.closeDrawer) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        drawer.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
+
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
+            drawer.closeDrawers();
+            return;
         }
+
+        // This code loads home fragment when back key is pressed
+        // when user is in other fragment than home
+        if ((shouldLoadHomeFragOnBackPress) && (navItemIndex != 0)) {
+            // checking if user is on other navigation menu
+            // rather than home
+            navItemIndex = 0;
+            CURRENT_TAG = TAG_HOME;
+            loadHomeFragment();
+            return;
+
+        }
+
+        super.onBackPressed();
     }
 
     @Override
@@ -72,21 +207,5 @@ public class MainActivity extends AppCompatActivity
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        }
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
---
+++ app/src/main/java/io/github/projektmedinf/wifisdcryptolocker/fragments/GalleryFragment.java
@@ -0,0 +1,32 @@
+package io.github.projektmedinf.wifisdcryptolocker.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import io.github.projektmedinf.wifisdcryptolocker.R;
+
+public class GalleryFragment extends android.support.v4.app.Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_gallery, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
